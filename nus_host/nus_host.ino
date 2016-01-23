6c1015470008445f390011277b4229aa7f42c39c
marcorussi <marco@marcorussi.net>
2016-01-23 23:18:34 +0000

    Host modified according to new multilink NUS client for nrf51 module

--- nus_host/nus_host.ino
+++ nus_host/nus_host.ino
@@ -1,5 +1,5 @@
 /* 
-  BLE CUBE - Marco Russi 
+  Arduino BLE NUS central host - Marco Russi 
 */
 
 /* ---------- Inclusions ---------- */
@@ -10,23 +10,15 @@
 
 /* Maximum number of found devices */
 #define MAX_NUM_FOUND_DEVICES     6
-/* Maximum number of target devices */
-#define MAX_NUM_TARGET_DEVICES    3
 
 /* BLE module RX pin */
 #define BLE_MODULE_RX_PIN         8  
 /* BLE module TX pin */ 
 #define BLE_MODULE_TX_PIN         9
-/* BLE module role pin - LOW is central, HIGH is peripheral */
-#define BLE_MODULE_ROLE_PIN       7   
 /* BLE module reset pin - active LOW */ 
 #define BLE_MODULE_RESET_PIN      6
 /* BLE module connection pin - LOW is disconnected, HIGH is connected */ 
 #define BLE_MODULE_CONN_PIN       5
-/* BLE module role pin value as peripheral */
-#define ROLE_PERIPH_PIN_VALUE     HIGH 
-/* BLE module role pin value as central */
-#define ROLE_CENTRAL_PIN_VALUE    LOW
 /* BLE module reset pin value as active */
 #define RESET_ACTIVE_PIN_VALUE    LOW 
 /* BLE module reset pin value as not active (released) */
@@ -42,35 +34,12 @@
 /* Software serial */
 SoftwareSerial mySerial(BLE_MODULE_RX_PIN, BLE_MODULE_TX_PIN);  /* RX, TX */
 
-/* Found devices addresses list */
-String found_dev_add[MAX_NUM_FOUND_DEVICES];
-/* Found devices names list */
-String found_dev_names[MAX_NUM_FOUND_DEVICES];
 /* Number of found devices */
 int num_found_devices = 0;  /* reset value */
 
-/* Target devices name strings */
-String target_devices[MAX_NUM_TARGET_DEVICES] = {
-  "ble_led_ctrl_02",
-  "ble_led_ctrl_03",
-  "ble_led_ctrl_04",
-};
+bool device_connected[MAX_NUM_FOUND_DEVICES];
 
-/* Devices name strings */
-String face_commands[6] = {
-  "1:B100-2:B020",
-  "1:B030-2:B080",
-  "",
-  "",
-  "",
-  ""
-};
-
-int dev_index_array[MAX_NUM_TARGET_DEVICES];  // TODO: init it in setup()
-
-bool device_connected[MAX_NUM_TARGET_DEVICES];
-
-boolean ble_module_central = true;
+static volatile bool data_mode = false;
 
 
 
@@ -84,24 +53,10 @@ void setup()
   
   /* set BLE module reset pin as output */
   pinMode(BLE_MODULE_RESET_PIN, OUTPUT);
-  /* set BLE module role pin as output */
-  pinMode(BLE_MODULE_ROLE_PIN, OUTPUT);
   /* Reset BLE module */
   digitalWrite(BLE_MODULE_RESET_PIN, RESET_ACTIVE_PIN_VALUE);
-  /* immediately set the default BLE role */
-  digitalWrite(BLE_MODULE_ROLE_PIN, ROLE_CENTRAL_PIN_VALUE);
-  /* start BLE module as central */
-  if(ble_module_central == true)
-  {
-    digitalWrite(BLE_MODULE_ROLE_PIN, ROLE_CENTRAL_PIN_VALUE);
-  }
-  /* or as peripheral */
-  else
-  {
-    digitalWrite(BLE_MODULE_ROLE_PIN, ROLE_PERIPH_PIN_VALUE);
-  }
-  /* wait for a while (50ms) */
-  delay(50);
+  /* wait for a while (100ms) */
+  delay(100);
   /* Release BLE module reset - start BLE module */
   digitalWrite(BLE_MODULE_RESET_PIN, RESET_RELEASED_PIN_VALUE);
 
@@ -114,204 +69,19 @@ void setup()
   /* set the data rate for the SoftwareSerial port */
   mySerial.begin(38400);
 
-
-  for(i=0; i<MAX_NUM_TARGET_DEVICES; i++)
+  /* clear connection flags */
+  for(i=0; i<MAX_NUM_FOUND_DEVICES; i++)
   {
     device_connected[i] = false;
   }
-
-
-
-
-  ble_scan_devices();
 }
 
 
 /* Loop function */
 void loop() 
 {
-  
-  /* get resp if present */
-  //ble_get_resp();
-
-  /* if BLE module is in central role */
-  if(ble_module_central == true)
-  {
-    /* rx and parse serial command */
-    rx_and_parse_cmd();
-  }
-  else
-  /* alse in peripheral role */
-  {
-    /* manage received data */
-    if (mySerial.available())
-    {
-      /* if connection pin indicates an established connection */
-      if( CONNECTED_PIN_VALUE == digitalRead(BLE_MODULE_CONN_PIN) )
-      {
-        Serial.write(mySerial.read());  // TO REMOVE
-        /* manage received data from peripheral role */
-        manage_periph_data();
-      }
-      /* else there is no connection */
-      else
-      {
-        /* discard received data */
-        mySerial.read();
-      }
-    }
-    else
-    {
-      /* do nothing */
-    }
-  }
-  
-}
-
-
-/* Function to manage data received from peripheral role */
-void manage_periph_data(void)
-{
-  /* do nothing at the moment */
-  /* TODO */
-}
-
-
-/* Function to execute all the preconfigured commands */
-void execute_controls(int face_index)
-{
-  int target_count, i, sep1, sep2;
-  int device_index;
-  String temp_string = "";
-  String command = "";
-  char cmd_string[20];
-  boolean end_of_cmd = false;
-  String temp_commands[MAX_NUM_TARGET_DEVICES];
-
-  /* scan devices */
-  //ble_scan_devices();
-
-  /* get command string */
-  temp_string += face_commands[face_index];
-  /* if command string is not null */
-  if( temp_string != "")
-  {
-    end_of_cmd = false;
-    sep2 = 0;
-    target_count = 0;
-    while(false == end_of_cmd)
-    {
-      sep1 = temp_string.indexOf(':', sep2);
-      device_index = (temp_string[sep1-1] & 0x0F);
-      sep2 = temp_string.indexOf('-', sep1);
-      /* if last substring */
-      if(sep2 == -1)
-      {
-        /* get last substring */
-        command += temp_string.substring(sep1+1);
-        end_of_cmd = true;
-      }
-      else
-      {
-        /* get substring */
-        command += temp_string.substring(sep1+1, sep2);
-      }
-
-      Serial.println("=========");
-      Serial.println(device_index);
-      temp_commands[device_index] += command;
-      Serial.println(temp_commands[device_index]);
-      command.remove(0);
-
-      if(device_connected[device_index] == false)
-      {
-        /* try to connect */
-        if(true == ble_connect_target_dev(dev_index_array[device_index]))
-        {
-          device_connected[device_index] = true;
-        }
-        else
-        {
-          /* fail to connect */
-        }
-      }
-      else
-      {
-        /* already connected */
-      }
-      delay(1000);
-      mySerial.write('*');
-      while(true != ble_get_resp());
-      delay(10);
-      /* next target */
-      target_count++;
-    }
-    temp_string.remove(0);
-
-
-    Serial.print("$$$$$$$$$ - ");
-    Serial.println(target_count);
-    //for(i=0; i<target_count; i++)
-    for(i=0; i<MAX_NUM_TARGET_DEVICES; i++)
-    {
-      /* if device is connected */
-      if(device_connected[i] == true)
-      {
-        Serial.print("switch");
-        sprintf(cmd_string, "AT+SWITCH=%d.", dev_index_array[i]);
-        mySerial.print(cmd_string);
-        while(true != ble_get_resp());
-        delay(100);
-        Serial.print("->");
-        Serial.println(temp_commands[i]);
-        mySerial.print(temp_commands[i]);
-        mySerial.write('.');
-        while(true != ble_get_resp());
-        delay(200);
-        mySerial.write('*');
-        while(true != ble_get_resp());
-      }
-      else
-      {
-        /* do nothing */
-      }
-    }
-
-
-    Serial.println("""""""""""");
-    //for(i=0; i<target_count; i++)
-    for(i=0; i<MAX_NUM_TARGET_DEVICES; i++)
-    {
-      /* if device is connected */
-      if(device_connected[i] == true)
-      {
-        /* drop connection without escaping, already done above */
-        ble_drop_connection(false, dev_index_array[i]);
-        Serial.print("drop:");
-        Serial.println(dev_index_array[i]);
-        /* device is disconnected */
-        device_connected[i] = false;
-      }
-      else
-      {
-        /* do nothing */
-      }
-    }
-  }
-  else
-  {
-    /* do nothing */
-    Serial.println("invalid face data");  // TO REMOVE
-  }
-}
-
-
-void rx_and_parse_cmd(void)
-{
   String uart_buffer;
-  String temp_string;
-  int target_dev_index;
-
+  
   if( Serial.available() > 0 ) 
   {
     /* copy each character into the buffer */
@@ -319,34 +89,164 @@ void rx_and_parse_cmd(void)
       char c = Serial.read();
       uart_buffer += c;
     } while (Serial.available());
-    
-    /* parse received command */
-    if(uart_buffer.startsWith("FACE:"))
+  
+    /* data mode */
+    if(data_mode == true)
     {
-      /* get required face index */
-      int pos = uart_buffer.indexOf(':');
-      /* consider one character only as index */
-      temp_string += uart_buffer[pos+1];
-      /* convert it into an integer */
-      target_dev_index = temp_string.toInt(); 
-      /* connect to the required target device */
-      Serial.print("face:"); // TO REMOVE
-      Serial.println(target_dev_index); // TO REMOVE
-      execute_controls(target_dev_index);
+      /* escape sequence */
+      if(uart_buffer.startsWith("***"))
+      {
+        delay(50);
+        mySerial.write('*');
+        while(true != ble_get_resp());
+        delay(50);
+        /* data mode disabled */
+        data_mode = false;
+      }
+      else
+      {
+        /* send data */
+        mySerial.print(uart_buffer);
+        /* consume eventual data response from module */
+        while(true != ble_get_resp());
+      }
     }
+    /* configuration mode */
     else
     {
-      /* invalid command; do nothing */
-      Serial.println("CRAP"); // TO REMOVE
+      /* rx and parse serial command */
+      rx_and_parse_cmd(uart_buffer);
     }
   }
   else
   {
-    /* no data received */
+    /* do nothing */
   }
 }
 
-int temp_dev_index = 0; // TODO: temporary solution
+
+void rx_and_parse_cmd(String uart_buffer)
+{
+  String temp_string;
+  int dev_index;
+  char cmd_string[20];
+
+    /* parse received command */
+    /* CONN:x */
+    if(uart_buffer.startsWith("CONN:"))
+    {
+      /* get required connection index */
+      int pos = uart_buffer.indexOf(':');
+      /* consider one character only as index */
+      temp_string += uart_buffer[pos+1];
+      /* convert it into an integer */
+      dev_index = temp_string.toInt();
+      /* if device is connected */
+      if((device_connected[dev_index] == false)
+      && (dev_index < num_found_devices)
+      && (num_found_devices != 0))
+      {
+        Serial.print("conn:"); // TO REMOVE
+        Serial.println(dev_index); // TO REMOVE
+        /* prepare connection command string */
+        sprintf(cmd_string, "AT+CONN=%d.", dev_index);
+        /* send command string */
+        mySerial.print(cmd_string);
+        Serial.println(cmd_string); // TO REMOVE
+        while(true != ble_get_resp());
+        /* device is connected */
+        device_connected[dev_index] = true;
+        /* data mode enabled */
+        data_mode = true;
+      }
+      else
+      {
+        /* already connected */
+        Serial.println("error");
+      }
+    }
+    /* SWITCH:x */
+    else if(uart_buffer.startsWith("SWITCH:"))
+    {
+      /* get required connection index */
+      int pos = uart_buffer.indexOf(':');
+      /* consider one character only as index */
+      temp_string += uart_buffer[pos+1];
+      /* convert it into an integer */
+      dev_index = temp_string.toInt(); 
+      /* if device is connected */
+      if((device_connected[dev_index] == true)
+      && (dev_index < num_found_devices)
+      && (num_found_devices != 0))
+      {
+        Serial.print("switch");
+        sprintf(cmd_string, "AT+SWITCH=%d.", dev_index);
+        mySerial.print(cmd_string);
+        while(true != ble_get_resp());
+        delay(100);
+        /* data mode enabled */
+        data_mode = true;
+      }
+      else
+      {
+        /* do nothing */
+        Serial.println("error");
+      }
+    }
+    /* DROP:x */
+    else if(uart_buffer.startsWith("DROP:"))
+    {
+      /* drop connection */
+      /* get required connection index */
+      int pos = uart_buffer.indexOf(':');
+      /* consider one character only as index */
+      temp_string += uart_buffer[pos+1];
+      /* convert it into an integer */
+      dev_index = temp_string.toInt(); 
+      /* if device is connected */
+      if((device_connected[dev_index] == true)
+      && (dev_index < num_found_devices)
+      && (num_found_devices != 0))
+      {
+        /* drop connection without escaping, already done above */
+        Serial.print("drop:");
+        Serial.println(dev_index);
+        /* send drop string */
+        sprintf(cmd_string, "AT+DROP=%d.", dev_index);
+        mySerial.print(cmd_string);
+        while(true != ble_get_resp());
+        /* device is disconnected */
+        device_connected[dev_index] = false;
+      }
+      else
+      {
+        /* do nothing */
+        Serial.println("error");
+      }
+    }
+    /* AUTO */
+    else if(uart_buffer.startsWith("AUTO"))
+    {
+      mySerial.print("AT+AUTO");
+
+      /* data mode enabled */
+      data_mode = true;
+    }
+    /* SCAN */
+    else if(uart_buffer.startsWith("SCAN"))
+    {
+      /* scan devices */
+      ble_scan_devices();
+    }
+    else
+    {
+      /* invalid command; do nothing */
+      Serial.println("WRONG");
+    }
+
+}
+
+
 /* Function to scan BLE devices */
 void ble_scan_devices(void)
 {
@@ -374,124 +274,13 @@ void ble_scan_devices(void)
   }
 
   /* get found devices info */
-  for(temp_dev_index=0; temp_dev_index<temp_max_num_dev; temp_dev_index++)
+  for(i=0; i<temp_max_num_dev; i++)
   {
     /* prepare command string to get found device info */
-    sprintf(cmd_string, "AT+FOUND=%d.", temp_dev_index);
+    sprintf(cmd_string, "AT+FOUND=%d.", i);
     mySerial.print(cmd_string);
     while(true != ble_get_resp());
   }
-
-
-  for(i=0; i<MAX_NUM_TARGET_DEVICES; i++)
-  {
-    temp_dev_index = 0;
-    while((found_dev_names[temp_dev_index] != target_devices[i]) 
-    &&    (temp_dev_index < num_found_devices))
-    {
-      temp_dev_index++;
-    }
-  
-    /* if device is not found */
-    if(temp_dev_index >= num_found_devices)
-    {
-      /* not found, set 0xFF */
-      dev_index_array[i] = 0xFF;
-    }
-    else
-    {
-      /* device found, valid index */
-      dev_index_array[i] = temp_dev_index;
-    }
-
-    Serial.print("->"); 
-    Serial.println(dev_index_array[i]); // TO REMOVE
-  }
-}
-
-
-/* Function to connect to a target device */
-// TODO: check connection index validity
-void ble_drop_connection(bool escape, int conn_index)
-{
-  char cmd_string[20];
-
-  /* send escape string if required */
-  if(escape == true)
-  {
-    /* send escape string */
-    mySerial.write('*');
-    /* wait for response */
-    while(true != ble_get_resp());
-  }
-  else
-  {
-    /* do not escape. It should be done before calling this function */
-  }
-  /* send drop string */
-  sprintf(cmd_string, "AT+DROP=%d.", conn_index);
-  mySerial.print(cmd_string);
-  while(true != ble_get_resp());
-}
-
-
-/* Function to connect to a target device */
-bool ble_connect_target_dev(int device_index)
-{
-  char cmd_string[20];
-  int found_dev_index;
-  bool success;
-
-  /* get found device index from required target device index */
-  //found_dev_index = get_found_dev_index(target_dev_index);
-  /* check device index validity */
-  //if(found_dev_index != 0xFF)
-  if(device_index < num_found_devices)
-  {
-    /* prepare connection command string */
-    sprintf(cmd_string, "AT+CONN=%d.", device_index);
-    /* send command string */
-    mySerial.print(cmd_string);
-    Serial.println(cmd_string); // TO REMOVE
-    while(true != ble_get_resp());
-    Serial.println("CONNESSO"); // TO REMOVE
-    success = true;
-  }
-  else
-  {
-    /* device is not found; do nothing */
-    Serial.println("DEVICE NOT FOUND");
-    success = false;
-  }
-
-  return success;
-}
-
-
-/* Function to get found device index */
-int get_found_dev_index(int target_dev_index)
-{
-  int dev_index = 0;
-
-  /* seek previously found device index corresponding to the required target device */
-  while((found_dev_names[dev_index] != target_devices[target_dev_index]) 
-  &&    (dev_index < num_found_devices))
-  {
-    dev_index++;
-  }
-
-  /* if device is not found */
-  if(dev_index >= num_found_devices)
-  {
-    /* return a "not found" code */
-    dev_index = 0xFF;
-  }
-  else
-  {
-    /* device found, valid index */
-  }
-  
-  return dev_index;
 }
 
 
@@ -502,7 +291,6 @@ boolean ble_get_resp(void)
 {
   int resp_length = 0;
   int sep;
-  int dev_index;
   String ble_resp_buffer;
   String temp_string = "";
   /* init as invalid response */
@@ -543,13 +331,6 @@ boolean ble_get_resp(void)
         /* consider as not valid in order to wait the next result response */
         Serial.println("wait");// TO REMOVE
       }
-      else if (ble_resp_buffer.startsWith("CONNECTED")) 
-      {
-        /* CONNECTED received */
-        /* valid response */
-        success = true;
-        Serial.println("connected");// TO REMOVE
-      }
       else if (ble_resp_buffer.startsWith("OK-")) 
       {
         temp_string += ble_resp_buffer.substring(3);
@@ -557,14 +338,12 @@ boolean ble_get_resp(void)
         {
           /* get device address */
           sep = temp_string.indexOf('-');
-          found_dev_add[temp_dev_index] += temp_string.substring(0, sep);
           temp_string.remove(0, sep+1);
-          Serial.println(found_dev_add[temp_dev_index]);
+          Serial.println(temp_string.substring(0, sep));
           /* get device name */
           sep = temp_string.indexOf('.');
-          found_dev_names[temp_dev_index] += temp_string.substring(0, sep);
           temp_string.remove(0, sep+1);
-          Serial.println(found_dev_names[temp_dev_index]);
+          Serial.println(temp_string.substring(0, sep));
         }
         else
         {
@@ -582,12 +361,6 @@ boolean ble_get_resp(void)
         Serial.println("ok");// TO REMOVE
         /* valid response */
         success = true;
-      }
-      else if (ble_resp_buffer.startsWith("SENT")) 
-      {
-        /* valid response */
-        success = true;
-        Serial.println("sent");// TO REMOVE
       }
       /* any other responses */
       else
